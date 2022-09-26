1e13fef82e44708ffb622d685e864f76ecb3165e
Assad <astb01@gmail.com>
2022-09-26 15:55:25 +0100

    Add session 1 content

---
+++ session-1/main.js
@@ -0,0 +1,13 @@
+console.log('hello world');
+
+// this is a single line comment
+
+/*
+    this is a 
+    multine comment
+    woohoooo
+*/
+
+// TODO: This should be fixed because it can be done using a function
+
+// bugfix-1234: 
